8d45b8c526e84663986f2140103e6db59757753e
Dave Inoc <michael828inoc@gmail.com>
2021-06-06 00:21:14 +0800

    test

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,7 +4,10 @@ import * as api from "../api/index.js";
 
 export const signin = (formData, history) => async (dispatch) => {
   try {
-    //log in the user..
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
+
     history.push("/");
   } catch (error) {
     console.log(error);
@@ -13,7 +16,10 @@ export const signin = (formData, history) => async (dispatch) => {
 
 export const signup = (formData, history) => async (dispatch) => {
   try {
-    //sign un the user..
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+
     history.push("/");
   } catch (error) {
     console.log(error);
